27722ba0678acb74b13d61cf78909126e6d2af90
kaiqi <550263774@qq.com>
2017-06-05 15:12:36 +0800

    Finsh login and logout

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,6 +18,10 @@ Route::get('/about','StaticPagesController@about')->name('about');
 Route::get('/signup','UsersController@create')->name('signup');
 Route::resource('users','UsersController');
 
+Route::get('login','SessionsController@create')->name('login');
+Route::post('login','SessionsController@store')->name('login');
+Route::delete('logout','SessionsController@destroy')->name('logout');
+
 // Route::get('/users', 'UsersController@index')->name('users.index');
 // Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 // Route::get('/users/create', 'UsersController@create')->name('users.create');
